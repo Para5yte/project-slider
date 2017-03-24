6b47e6b2850689fda519dac523d5223bfee70f37
Para5yte <tmitsu195@gmail.com>
2017-03-24 14:25:55 +1100

    updated one

--- Board.java
+++ Board.java
@@ -1,22 +1,24 @@
-
+/** The Game Board
+ */
 public class Board {
 	private int boardSize;
 	private GamePiece[][] boardState;
 	
-	
+	/** initialting a new Board */
 	public Board(int size){
 		this.boardSize = size;
 		this.boardState = new GamePiece[boardSize][boardSize];
 	}
 	
-	
-	public void initiateBoard(){
-		//for(int i; )
-	}
-	
+	/** Marks the board with a given content
+	 * @param content The charater value of the piece
+	 * @param x The x coordinate of the given content
+	 * @param y The y coordinate of the given content
+	 */
 	public void markBoard(char content, int x, int y){
 		GamePiece piece = null;
 		
+		/* Only four types of game pieces in the game */
 		switch(content){
 			case 'H': 
 				piece = new Horizontal();
@@ -37,31 +39,25 @@ public class Board {
 		this.boardState[x][y] = piece;
 	}
 	
-	public void printBoard(){
-		for(int i = 0; i < boardSize; i++){
-			
-			for(int j = 0; j < boardSize; j++){
-				
-				System.out.print(boardState[i][j].toString() + ' ');
-			}
-			System.out.print("\n");
-		}
-	}
-	
+	/** Checks if the given coordinates are out of bound from the board
+	 *  @param x The x coordinate
+	 *  @param y The y coordinate
+	 */ 
 	public boolean outOfBond(int x, int y){
 		return x < 0 || x >= boardSize || y < 0 || y >= boardSize;
 	}
 	
+	/** returns the piece thats in the position of the given coordinates
+	 * @param x The x coordinate
+	 * @param y The y coordinate
+	 */
 	public GamePiece getPiece(int x, int y){
 		return boardState[x][y];
 	}
 	
+	/** returns the board size
+	 */
 	public int getBoardSize() {
 		return boardSize;
-	}
-
-
-	public void setBoardSize(int boardSize) {
-		this.boardSize = boardSize;
 	}
 }
